7129ce3ce78d952caa9ddd04da8d4ffea106e34d
Tony Rizko <tony@rizkocircle.com>
2023-09-06 22:07:36 -0700

    Add touch/click input handling to ball controller

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
     void Start()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.down * 10f;
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        rb.velocity = (Vector2.up + Vector2.right) * 10f;
+    }
+
+    void OnTouch(InputValue value)
+    {
+        rb.velocity = Vector2.zero;
+        sr.color = Color.green;
+        Debug.Log($"ball position: {gameObject.transform.position}");
+        Debug.Log($"touch position: {Camera.main.ScreenToWorldPoint(value.Get<Vector2>())}");
     }
 }
