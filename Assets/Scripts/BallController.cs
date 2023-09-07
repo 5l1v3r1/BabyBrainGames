ac92021c78d5ed93cb7939bf03066a168fd423f5
Tony Rizko <tony@rizkocircle.com>
2023-09-06 20:21:06 -0700

    Add velocity to ball object at start of scene

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.down * 20f;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb.velocity = Vector2.down * 10f;
     }
 }
