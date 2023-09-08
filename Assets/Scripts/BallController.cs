351589da82e088d9d3571933b710b9eebf2833ab
Tony Rizko <tony@rizkocircle.com>
2023-09-07 21:31:29 -0700

    Update ball controller to use c# events input actions

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,19 +5,37 @@ public class BallController : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer sr;
+    PlayerInput playerInput;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        playerInput = new PlayerInput();
+
+        playerInput.BallCatchInputs.Touch.started += onTouch;
+    }
+
+    void Start()
+    {
         rb.velocity = (Vector2.up + Vector2.right) * 10f;
     }
 
-    void OnTouch(InputValue value)
+    void onTouch(InputAction.CallbackContext context)
     {
         rb.velocity = Vector2.zero;
         sr.color = Color.green;
         Debug.Log($"ball position: {gameObject.transform.position}");
-        Debug.Log($"touch position: {Camera.main.ScreenToWorldPoint(value.Get<Vector2>())}");
+        Debug.Log($"touch position: {Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>())}");
+    }
+
+    void OnEnable()
+    {
+        playerInput.BallCatchInputs.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInput.BallCatchInputs.Disable();
     }
 }
